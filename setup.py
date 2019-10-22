1705c64b47b0e4e04d18779016cf30745477bf65
ivan <iiglesias@gisce.net>
2019-10-22 10:48:32 +0200

    Add requirements.txt to setup

--- setup.py
+++ setup.py
@@ -2,10 +2,15 @@
 from setuptools import setup, find_packages
 from measuring_tools import __version__
 
+with open('requirements.txt') as f:
+    data = f.read()
+reqs = data.split()
+
 setup(
     name='measuring-tools',
     version=__version__,
     packages=find_packages(),
+    install_requires=reqs,
     license='GNU GPL3',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
